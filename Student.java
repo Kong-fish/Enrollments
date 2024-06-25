203e872d376f4868934052187c845066015fa984
ChongQi04 <134798345+ChongQi04@users.noreply.github.com>
2024-06-25 12:47:15 +0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private List<Course> courses;
+    private boolean needsAccommodation;
+
+    public Student(String name) {
+        this.name = name;
+        this.courses = new ArrayList<>();
+        this.needsAccommodation = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void addCourse(Course course) {
+        courses.add(course);
+    }
+
+    public boolean needsAccommodation() {
+        return needsAccommodation;
+    }
+
+    public void setNeedsAccommodation(boolean needsAccommodation) {
+        this.needsAccommodation = needsAccommodation;
+    }
+}
